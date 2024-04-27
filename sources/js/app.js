0e135a8398033a6f7e2ebfacd562779b1d8c9b23
andresH <andreshg9203@gmail.com>
2024-04-27 15:18:43 -0400

    feat: working with him DOM

--- sources/js/app.js
+++ sources/js/app.js
@@ -3,30 +3,47 @@
  * 1- crear function verificadora si es o no un palindromo
  * 2- crear variables que agreguen y modifiquen el dom  
 */
+//FUNCIONES
+const idRespuesta = document.getElementById('respuesta');
 
+const numPal = (n) => {
+    
+    
+    if(n.length >= 5){
+      
+        let strPal = n.toString().split('').reverse().join('');
+    
+        if(Number(n) === Number(strPal)){
 
-const numPal = (num) => {
-    let strNum = Number(num);
-    if(typeof strNum === 'number' && num.length >= 5){
-        //conversion tipo de dato
-        let strPal = strNum.toString().split('').reverse().join('');
-
-        if(strPal === num){
-            console.log(`El numero ${num}, SI es un palindromo`)
-            //parrafo que muestre si es o no un palindromo
-            return document.getElementById('respuesta').innerHTML = `Si es un palindromo...!`;
-        } else {
-            console.log(`El numero ${num}, NO es un palindromo`)
-            return document.getElementById('respuesta').innerHTML = `No es un palindromo...!`;
-        }
-    } else {
-        return document.getElementById('respuesta').innerHTML = `Le faltan cifras o no es un numero...!`;
+            return idRespuesta.innerHTML = `<strong class='bg-success fw-bolder text-light border border-black rounded p-2'>
+                                            ${n}</strong> SI ES UN PALINDROMO...!`;
+        
+        }else if(Number(n) !== Number(strPal)){
+        
+            return idRespuesta.innerHTML = `<strong class='bg-warning fw-bolder text-dark border border-black rounded p-2'>
+                                            ${n}</strong> NO ES UN PALINDROMO...!`;
+        
+        } 
+    } else if (n.length > 1 && n.length < 5){
+        return idRespuesta.innerHTML = `<strong class='bg-danger text-light border border-black rounded p-2'>
+                                        ${n}</strong> LE FALTAN NUMEROS A TU PALINDROMO...!`;
+    } else if(n.length <= 1){
+        return idRespuesta.innerHTML = `<strong class='p-2 rounded fw-bolder bg-secondary text-dark'> ESCRIBE TU PALINDROMO...! </strong>`;
     }
 }
 
-const idInputPal = document.getElementById('inpPal');
-const idBtnPal = document.getElementById('btnPal');
 
+const namePromp = () => {
+    let name = prompt('Cual es tu nombre ?');
+    return name.toUpperCase();
+}
+
+const idInputPal = document.getElementById('inpPal');
+const idName = document.getElementById('name');
+idName.innerText = namePromp();
+
+
+//EVENTOS
 idInputPal.addEventListener('change', (e) => {
     e.preventDefault();
     numPal(e.target.value)
